ff07380cbc6663053a4709ce15134d48a331cad5
lgabelashvili <gabelashvili1999@gmail.com>
2025-10-12 15:40:01 +0400

    Initialize React + TypeScript + Vite project with ESLint and Tailwind CSS setup. Added essential configuration files, including package.json, tsconfig, and eslint.config.js. Created basic application structure with App component and index files. Included .gitignore for build artifacts and logs.

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: true,
+          jsxSingleQuote: false,
+          trailingComma: "all",
+          printWidth: 120,
+          tabWidth: 2,
+          bracketSpacing: true,
+          arrowParens: "always",
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+]).concat(eslintPluginPrettier);
